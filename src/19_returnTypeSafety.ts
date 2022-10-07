6b9106681323d7595ab221c9c8c14ee090e38543
kjkandrea <kjkandrea@gmail.com>
2022-10-07 14:26:56 +0900

    feat: 리턴타입을 명시하지 않음으로 발생하는 에러 재현

---
+++ src/19_returnTypeSafety.ts
@@ -0,0 +1,15 @@
+const cache: {[ticker: string]: number} = {};
+function getQuote(ticker: string) {
+  if (ticker in cache) {
+    return cache[ticker];
+  }
+  return fetch(`https://quotes.example.com/q=${ticker}`)
+    .then(response => response.json())
+    .then(quote => {
+      cache[ticker] = quote;
+      return quote;
+    });
+}
+
+// 문제. getQuote 가 4열에 의해 Promise<number> 를 반환하지 않으므로 에러
+getQuote('MSFT').then(console.log);

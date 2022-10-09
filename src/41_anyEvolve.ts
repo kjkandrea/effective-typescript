1e13ca123bf38606fbdb3f131fc0adddc665f6fc
kjkandrea <kjkandrea@gmail.com>
2022-10-09 13:50:04 +0900

    feat: any 의 진화 안쓰는 예제 추가.

--- src/41_anyEvolve.ts
+++ src/41_anyEvolve.ts
@@ -5,3 +5,12 @@ function range(start: number, limit: number) {
   }
   return out; // 타입이 number[]
 }
+
+// 이게 더 안전한 설계
+function range2(start: number, limit: number) {
+  const out: number[] = [];
+  for (let i = start; i < limit; i++) {
+    out.push(i);
+  }
+  return out; // 타입이 number[]
+}
